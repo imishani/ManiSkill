53fa8bbe1cc9b147a1fccd0330f9541c9dde96aa
Itamar Mishani <imishani@gmail.com>
2024-12-18 18:05:44 -0500

    dev(pick_block)

--- mani_skill/examples/motionplanning/ridgebackur10e/solutions/pick_ycb.py
+++ mani_skill/examples/motionplanning/ridgebackur10e/solutions/pick_ycb.py
@@ -3,7 +3,7 @@ import sapien
 import torch
 import trimesh
 
-from mani_skill.envs.tasks import PickSingleKitchenYCBEnv
+from mani_skill.envs.tasks import PickSingleKitchenYCBEnv, PickHeavyClutterYCBEnv, PickCubeEnv, PickBlockEnv
 from mani_skill.examples.motionplanning.ridgebackur10e.motionplanner import \
     RidgebackUR10ePlanningSolver
 from mani_skill.examples.motionplanning.panda.utils import (
@@ -12,7 +12,8 @@ from mani_skill.utils.common import quat_diff_rad
 from mani_skill.utils.geometry.rotation_conversions import quaternion_to_matrix
 
 
-def solve(env: PickSingleKitchenYCBEnv, seed=None, debug=False, vis=False):
+def solve(env: PickSingleKitchenYCBEnv | PickHeavyClutterYCBEnv | PickCubeEnv | PickBlockEnv,
+          seed=None, debug=False, vis=False):
     env.reset(seed=seed)
     planner = RidgebackUR10ePlanningSolver(
         env,
@@ -23,15 +24,26 @@ def solve(env: PickSingleKitchenYCBEnv, seed=None, debug=False, vis=False):
         print_env_info=False,
     )
 
-
+    ind_env = 0 # Not parallelized on gpu
     # torch_planer =
 
     FINGER_LENGTH = 0.025
     env = env.unwrapped
 
+    if isinstance(env, PickCubeEnv):
+        target = env.cube
+    elif isinstance(env, PickSingleKitchenYCBEnv):
+        target = env.obj
+    elif isinstance(env, PickHeavyClutterYCBEnv):
+        target = env.target_object
+    else:
+        raise ValueError("Unknown environment type")
+
     # loop through the objects in the scene, and if they are not the object of interest, add them to the planner
     for actor in env.scene.actors.values():
-        if not actor.name.startswith(env.model_id):
+        if actor.per_scene_id[ind_env] != target.per_scene_id[ind_env]:
+        # if actor.per_scene_id[ind_env] != env.obj.per_scene_id[ind_env]:
+        # if not actor.name.startswith(env.model_id):
             # get collision meshes
             meshes = actor.get_collision_meshes()
             for mesh in meshes:
@@ -41,9 +53,11 @@ def solve(env: PickSingleKitchenYCBEnv, seed=None, debug=False, vis=False):
                 # to numpy
                 ptc = np.array(ptc)
                 planner.add_collision_pts(ptc)
-    get_ooi = lambda x: next(y for y in x if y.name.startswith(env.model_id))
+    # get_ooi = lambda x: next(y for y in x if y.name.startswith(env.model_id))
+    get_ooi = lambda x: next(y for y in x if y.per_scene_id[ind_env] == target.per_scene_id[ind_env])
+    # get_ooi = lambda x: next(y for y in x if y.per_scene_id[ind_env] == env.obj.per_scene_id[ind_env])
     ooi = get_ooi(env.scene.actors.values())
-
+    planner.render_wait()
     grasp_pose = planner.find_best_grasp(ooi)
 
     qpose = grasp_pose[1]
@@ -86,6 +100,7 @@ def solve(env: PickSingleKitchenYCBEnv, seed=None, debug=False, vis=False):
 
 
     res = planner.move_to_pose_with_RRTConnect(goal_pose, constrain=True)
+    # res = planner.move_to_pose_with_RRTConnect(goal_pose, constrain=False)
     # res = planner.move_to_pose_with_screw(goal_pose)
     if res == -1:
         planner.close()
