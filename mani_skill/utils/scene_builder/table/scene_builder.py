f15ea4697fca739b70127544931e3879705a9985
Itamar Mishani <imishani@gmail.com>
2024-12-23 10:03:13 -0500

    pick block

--- mani_skill/utils/scene_builder/table/scene_builder.py
+++ mani_skill/utils/scene_builder/table/scene_builder.py
@@ -180,18 +180,30 @@ class TableSceneBuilder(SceneBuilder):
                 group=2, bit_idx=RIDGEBACK_WHEELS_COLLISION_BIT, bit=1
             )
         elif self.env.robot_uids == "static_ridgebackur10e":
-            # qpos = np.array(
-            #     [0., -1.0472, -2., 0., -3.*np.pi/2., 0.,
-            #      0.8, 0.8,
-            #      0.8, 0.8, -0.8, -0.8  # Passive joints for the gripper
-            #      ]
-            # )
             qpos = np.array(
-                [0., -np.pi/2., -np.pi/2., -np.pi/2., -3. * np.pi / 2., 0.,
+                [0., -1.0472, -2., 0., -3.*np.pi/2., 0.,
                  0.8, 0.8,
                  0.8, 0.8, -0.8, -0.8  # Passive joints for the gripper
                  ]
             )
+            ###### push block #######
+            # qpos = np.array(
+            #     [0., -np.pi/2., -np.pi/2., -np.pi/2., -3. * np.pi / 2., 0.,
+            #      0.8, 0.8,
+            #      0.8, 0.8, -0.8, -0.8  # Passive joints for the gripper
+            #      ]
+            # )
+            #########################
+            # ##### pick block #######
+            # qpos = np.array(
+            #     [0., -0.95, -2.752, -1.0, np.pi/2, -np.pi/2.,
+            #      0., 0.,
+            #      0., 0., -0., -0.  # Passive joints for the gripper
+            #     ]
+            # )
+            # ########################
+
+
             self.env.agent.reset(qpos)
             self.env.agent.robot.set_pose(sapien.Pose([-1.2, 0, -self.table_height]))
             self.ground.set_collision_group_bit(
